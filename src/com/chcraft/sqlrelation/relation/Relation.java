3e3b17cf3e2ef4a7b8f6280db8719c1f5e89389a
박찬하 <chanhacraft@naver.com>
2021-02-19 14:37:46 +0900

    [ADD] constructor, getter and setter

--- src/com/chcraft/sqlrelation/relation/Relation.java
+++ src/com/chcraft/sqlrelation/relation/Relation.java
@@ -5,4 +5,38 @@ import com.chcraft.sqlrelation.entity.Entity;
 public class Relation {
 	private Entity origin;
 	private Entity foreign;
+	private RelationType relationType;
+
+	public Relation() {
+	}
+
+	public Relation(Entity origin, Entity foreign, RelationType relationType) {
+		this.origin = origin;
+		this.foreign = foreign;
+		this.relationType = relationType;
+	}
+
+	public Entity getOrigin() {
+		return origin;
+	}
+
+	public void setOrigin(Entity origin) {
+		this.origin = origin;
+	}
+
+	public Entity getForeign() {
+		return foreign;
+	}
+
+	public void setForeign(Entity foreign) {
+		this.foreign = foreign;
+	}
+
+	public RelationType getRelationType() {
+		return relationType;
+	}
+
+	public void setRelationType(RelationType relationType) {
+		this.relationType = relationType;
+	}
 }
