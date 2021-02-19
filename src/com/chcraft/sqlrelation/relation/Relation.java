0df5231d547b9742279ac1e1aa87bf6e912b7a41
박찬하 <chanhacraft@naver.com>
2021-02-19 13:42:14 +0900

    merge feature/entity and relation

--- src/com/chcraft/sqlrelation/relation/Relation.java
+++ src/com/chcraft/sqlrelation/relation/Relation.java
@@ -1,6 +1,6 @@
 package com.chcraft.sqlrelation.relation;
 
-import com.chcraft.sqlrelation.Entity;
+import com.chcraft.sqlrelation.entity.Entity;
 
 public class Relation {
 	private Entity origin;
