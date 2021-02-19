b634c16492cf24fb0a4fbffc6cda0a25121ea675
박찬하 <chanhacraft@naver.com>
2021-02-19 15:05:36 +0900

    ADD getter and setter to class Column

--- src/com/chcraft/sqlrelation/entity/Column.java
+++ src/com/chcraft/sqlrelation/entity/Column.java
@@ -3,4 +3,28 @@ package com.chcraft.sqlrelation.entity;
 public class Column {
 	private String name;
 	private String datatype;
+
+	public Column() {
+	}
+
+	public Column(String name, String datatype) {
+		this.name = name;
+		this.datatype = datatype;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDatatype() {
+		return datatype;
+	}
+
+	public void setDatatype(String datatype) {
+		this.datatype = datatype;
+	}
 }
