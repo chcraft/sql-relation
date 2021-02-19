3fd4a1004d993288c59fb778b18d1e789cc9f762
박찬하 <chanhacraft@naver.com>
2021-02-19 14:18:27 +0900

    change class Entity -> interface Entity, make class Column

--- src/com/chcraft/sqlrelation/entity/Entity.java
+++ src/com/chcraft/sqlrelation/entity/Entity.java
@@ -1,56 +1,9 @@
 package com.chcraft.sqlrelation.entity;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 
-public class Entity {
-	private String name;
-	private Map<String, String> columns;
-	private Map<String, String> foreignKeys;
-
-	public Entity() {
-		this.columns = new HashMap<>();
-		this.foreignKeys = new HashMap<>();
-	}
-
-	public Entity(String name) {
-		this.name = name;
-		this.columns = new HashMap<>();
-		this.foreignKeys = new HashMap<>();
-	}
-
-	public Entity(String name, Map<String, String> columns) {
-		this.name = name;
-		this.columns = columns;
-	}
-
-	public Entity(String name, Map<String, String> columns, Map<String, String> foreignKeys) {
-		this.name = name;
-		this.columns = columns;
-		this.foreignKeys = foreignKeys;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public Map<String, String> getColumns() {
-		return columns;
-	}
-
-	public void setColumns(Map<String, String> columns) {
-		this.columns = columns;
-	}
-
-	public Map<String, String> getForeignKeys() {
-		return foreignKeys;
-	}
-
-	public void setForeignKeys(Map<String, String> foreignKeys) {
-		this.foreignKeys = foreignKeys;
-	}
+public interface Entity {
+	String getName();
+	String getPrimaryKey();
+	List<Column> getColumns();
 }
