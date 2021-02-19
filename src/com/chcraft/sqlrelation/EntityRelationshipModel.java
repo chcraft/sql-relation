52d4b95f81aeeb8628ffc2182a04a40c18e22390
박찬하 <chanhacraft@naver.com>
2021-02-19 16:16:20 +0900

    make class EntityRelationshipModel

---
+++ src/com/chcraft/sqlrelation/EntityRelationshipModel.java
@@ -0,0 +1,11 @@
+package com.chcraft.sqlrelation;
+
+import java.util.List;
+
+import com.chcraft.sqlrelation.entity.Entity;
+import com.chcraft.sqlrelation.relation.Relation;
+
+public class EntityRelationshipModel {
+	private List<Entity> entities;
+	private List<Relation> relations;
+}
