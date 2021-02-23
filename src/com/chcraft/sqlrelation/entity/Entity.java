b83122d2a33f46b1826bd71239525ea91cf6f358
박찬하 <chanhacraft@naver.com>
2021-02-23 10:49:36 +0900

    MODIFY interface Entity -> class Entity

--- src/com/chcraft/sqlrelation/entity/Entity.java
+++ src/com/chcraft/sqlrelation/entity/Entity.java
@@ -1,9 +1,38 @@
 package com.chcraft.sqlrelation.entity;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public interface Entity {
-	String getName();
-	String getPrimaryKey();
-	List<Column> getColumns();
+public class Entity {
+	private String name;
+	private List<Column> columns;
+
+	public Entity() {
+		this.columns = new ArrayList<>();
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<Column> getColumns() {
+		return columns;
+	}
+
+	public void setColumns(List<Column> columns) {
+		this.columns = columns;
+	}
+
+	public Column getPrimaryKey() {
+		// TODO : not implemented method.
+		return null;
+	}
+
+	public boolean addColumn(Column column) {
+		return this.columns.add(column);
+	}
 }
