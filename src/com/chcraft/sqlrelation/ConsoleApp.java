0c35e7123f6b8800a515254f1af52cc6ce63ca3a
박찬하 <chanhacraft@naver.com>
2021-02-26 12:02:49 +0900

    .

--- src/com/chcraft/sqlrelation/ConsoleApp.java
+++ src/com/chcraft/sqlrelation/ConsoleApp.java
@@ -1,10 +1,56 @@
 package com.chcraft.sqlrelation;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class ConsoleApp {
 	public static void main(String[] args) {
-		EntityRelationshipModel model = new EntityRelationshipModel();
+		List<EntityRelationshipModel> models = new ArrayList<EntityRelationshipModel>();
 
-		System.out.println("SQL RELATION MODEL MAKER(demo)");
+		System.out.println("SQL RELATION MODEL MAKER");
 		System.out.println();
+		while(true) {
+			int command;
+			showCommandList();
+			command = readCommand();
+			if(!executeCommand(command))
+				break;
+		}
+		System.out.println("Bye~");
+	}
+
+	private static boolean executeCommand(int command) {
+		switch(command) {
+		case 1:
+			return true;
+		case 2:
+			return true;
+		case 3:
+			return true;
+		case 4:
+			return false;
+		}
+		System.out.println("invalid command");
+		return true;
+	}
+
+	private static void showCommandList() {
+		System.out.println("--COMMANDS---------------------------------");
+		System.out.println("|1. Show Entity Relationship Model List   |");
+		System.out.println("|2. Create New Entity Relationship Model  |");
+		System.out.println("|3. Modify Entity Relationship Model      |");
+		System.out.println("|4. Quit                                  |");
+		System.out.println("-------------------------------------------");
+	}
+
+	private static int readCommand() {
+		int input = 0;
+
+		try(Scanner sc = new Scanner(System.in);){
+			input = sc.nextInt();
+		}
+
+		return input;
 	}
 }
